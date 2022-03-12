283d160e8ca5d577c61db407b854842edb5c5279
matharva <atharvamohite20@gmail.com>
2022-03-13 03:10:35 +0530

    2 more pages done

--- pages/poll/index.tsx
+++ pages/poll/index.tsx
@@ -12,28 +12,37 @@ const Poll = () => {
           What is your favourite movie?
         </div>
         {/* Options */}
-        <div className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200">
+        <div
+          className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200"
+          style={{ borderWidth: "3px" }}
+        >
           <div className="border-2 border-green-400 p-2 rounded-full ml-4 flex items-center justify-center bg-green-400">
             <BsCheckLg color="white" className="text-sm" />
           </div>
           <div className="text-xl pl-4">option1</div>
         </div>
-        <div className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200">
-          <div className="border-2 p-2 rounded-full ml-4 flex items-center justify-center bg-green-400">
+        <div
+          className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200"
+          style={{ borderWidth: "3px" }}
+        >
+          <div className="border-2 border-green-400 p-2 rounded-full ml-4 flex items-center justify-center bg-green-400">
             <BsCheckLg color="white" className="text-sm" />
           </div>
           <div className="text-xl pl-4">option1</div>
         </div>
-        <div className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200">
-          <div className="border-2 p-2 rounded-full ml-4 flex items-center justify-center bg-green-400">
+        <div
+          className="flex items-center justify-start border-2 p-4 rounded-md border-green-400 my-6 shadow-lg shadow-green-200"
+          style={{ borderWidth: "3px" }}
+        >
+          <div className="border-2 border-green-400 p-2 rounded-full ml-4 flex items-center justify-center bg-green-400">
             <BsCheckLg color="white" className="text-sm" />
           </div>
           <div className="text-xl pl-4">option1</div>
         </div>
-        <div className="bg-green-400 p-4 w-full text-center rounded font-bold text-white text-xl my-8 mt-12 max-w-lg md:mx-auto">
+        <div className="bg-green-400 p-4 w-full text-center rounded font-bold text-white text-xl my-4 mt-12 max-w-lg md:mx-auto">
           Submit your vote
         </div>
-        <div className="bg-gray-300 p-4 w-full text-center rounded font-bold text-white text-xl my-8  max-w-lg md:mx-auto">
+        <div className="bg-gray-300 p-4 w-full text-center rounded font-bold text-white text-xl my-6 max-w-lg md:mx-auto">
           Jump to result {">"}
         </div>
       </div>

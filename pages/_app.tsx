8e884571966e42a33ab64d777e9a42be98fd0b67
matharva <atharvamohite20@gmail.com>
2022-03-13 21:48:17 +0530

    theme refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,28 @@ import "../styles/globals.css";
 import "./styles.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { extendTheme } from "@chakra-ui/react";
+
+const overrides = extendTheme({
+  styles: {
+    global: (props: any) => ({
+      body: {
+        fontFamily: "Inter",
+      },
+      html: {
+        fontFamily: "Inter",
+      },
+    }),
+  },
+});
+
+const customTheme = extendTheme(overrides);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={customTheme}>
       <div className="grid-parent">
         <Navbar />
         <div className="main-body">

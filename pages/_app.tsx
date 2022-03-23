8adf3116d6d91c141afeb65db0a10dde6ba473bc
matharva <atharvamohite20@gmail.com>
2022-03-24 00:36:49 +0530

    http-equiv error solved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,7 +50,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel="manifest" href="/manifest.json" />
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"

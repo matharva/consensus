f36b020c2d66d040a894bd91d409201bf07089b8
matharva <atharvamohite20@gmail.com>
2022-03-24 00:53:22 +0530

    font bug solved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -56,6 +56,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="description" content="description of your project" />
         <meta name="theme-color" content="#000" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com"> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <div className="grid-parent">
         <Navbar />
